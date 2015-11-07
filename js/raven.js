1029f7b07d0854fad401b11225add6e57d82384f
Alex Lam <alexlam1994@gmail.com>
2015-11-07 12:26:33 -0500

    Patient List

---
+++ js/raven.js
@@ -0,0 +1,20 @@
+function submitDoctorForm(){
+    var institution = document.getElementById("institution-field").value;
+     var degree = document.getElementById("degree-field").value;
+     var grad = document.getElementById("grad-field").value;
+     var state = document.getElementById("state-field").value;
+    
+    //get facebook id or whatever identifier was made
+    
+    window.location = "http://www.yoururl.com";
+    
+}
+
+function populatePatients(){
+    
+    //Call server to get JSON response of patient data
+    
+    //foreach patient, get the picture, name and profile ID
+    
+    
+}
